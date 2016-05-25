0aa93e194be770b9348df7bdd05c20801d815785
nakai <crew01@trym.jp>
2016-05-25 19:00:52 +0900

    160525nakai

--- common/js/common.js
+++ common/js/common.js
@@ -302,3 +302,50 @@ common scripts
     });
 
 });
+
+
+// placeholder
+$(function(){
+    $(window).load(function(){
+        $('.place').each(function(){
+            var thisTitle = $(this).attr('title');
+            if(!(thisTitle === '')){
+                $(this).wrapAll('<span style="text-align:left;display:inline-block;position:relative;"></span>');
+                $(this).parent('span').append('<span class="placeholder">' + thisTitle + '</span>');
+                $('.placeholder').css({
+                    top:'4px',
+                    left:'5px',
+                    fontSize:'100%',
+                    lineHeight:'120%',
+                    textAlign:'left',
+                    color:'#A9A9A9',
+                    overflow:'hidden',
+                    position:'absolute',
+                    zIndex:'99'
+                }).click(function(){
+                    $(this).prev().focus();
+                });
+
+                $(this).focus(function(){
+                    $(this).next('span').css({display:'none'});
+                });
+
+                $(this).blur(function(){
+                    var thisVal = $(this).val();
+                    if(thisVal === ''){
+                        $(this).next('span').css({display:'inline-block'});
+                    } else {
+                        $(this).next('span').css({display:'none'});
+                    }
+                });
+
+                var thisVal = $(this).val();
+                if(thisVal === ''){
+                    $(this).next('span').css({display:'inline-block'});
+                } else {
+                    $(this).next('span').css({display:'none'});
+                }
+            }
+        });
+    });
+});
