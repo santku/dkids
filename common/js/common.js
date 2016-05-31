edc00111fa5ee7af0be95cb01deb3cbe439f5a33
nakai <crew01@trym.jp>
2016-05-31 19:20:51 +0900

    16053119 nakai

--- common/js/common.js
+++ common/js/common.js
@@ -465,3 +465,24 @@ new function(){
 	addEvent(window,"resize",footerFixed);
 
 }
+
+
+// loding
+$(function() {
+  var h = $(window).height();
+
+  $('#wrap').css('display','none');
+  $('#loader-bg ,#loader').height(h).css('display','block');
+});
+
+$(window).load(function () {
+  $('#loader-bg').delay(0).fadeOut(100);
+  $('#loader').delay(0).fadeOut(100);
+  $('#wrap').css('display', 'block');
+});
+
+function stopload(){
+  $('#wrap').css('display','block');
+  $('#loader-bg').delay(900).fadeOut(800);
+  $('#loader').delay(600).fadeOut(300);
+}
